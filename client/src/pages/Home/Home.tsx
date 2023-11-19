3fd5329ea142ebecedf3f8e4f5b92c20f25913c3
teodorazhelyazkova <teodora.s.zhelyazkova@gmail.com>
2023-11-19 21:04:58 +0200

    add google maps to home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,12 +1,23 @@
 import { FC } from 'react';
-import { observer } from 'mobx-react-lite';
 import { Typography } from '@mui/material';
+import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+import { GOOGLE_MAPS_KEY } from '../../configs/environment';
+
 import styles from './Home.module.scss';
 
-export const Home: FC = observer(() => {
+export const Home: FC = () => {
+    const position = { lat: 42.69174, lng: 23.32436 };
+
     return (
-        <section className={styles.Home}>
-            <Typography variant='h2'>Welcome to the Library!</Typography>
-        </section>
+        <APIProvider apiKey={GOOGLE_MAPS_KEY}>
+            <section className={styles.Home}>
+                <Typography variant='h2' sx={{ alignSelf: 'center' }}>
+                    Welcome to the Library!
+                </Typography>
+                <Map zoom={18} center={position}>
+                    <Marker position={position} />
+                </Map>
+            </section>
+        </APIProvider>
     );
-});
+};
